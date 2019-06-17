0dfa3c027b4259c069b1245717028a4e6b74d8ec
Ruben R. Kazumov <k@r.attlocal.net>
2019-06-16 22:55:15 -0500

    initial

---
+++ ftpa.R
@@ -0,0 +1,10 @@
+#
+# Future transactions prediction algorithm
+# Author: Ruben R. Kazumov <kazumov@gmail.com>
+# License: MIT
+#
+
+library(tidyverse)
+library(lubridate)
+library(ggplot2)
+library(randomForest)
